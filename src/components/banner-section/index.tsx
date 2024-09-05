4a65ddc331ece56e0681ea0e1aa092200b8818d6
João Lucas MAIN <joaolucasmmd@gmail.com>
2024-09-05 00:56:46 -0300

    feat: add banner section component

---
+++ src/components/banner-section/index.tsx
@@ -0,0 +1,21 @@
+import styles from "./banner-section.module.css";
+import { Button } from "../buttons";
+
+
+import BannerBackground from "@/assets/images/banner.svg"
+
+export function BannerSection() {
+    return (
+        <div className={styles.bannerContainer}>
+            <div className={styles.bannerSection} style={{
+            backgroundImage: `url(${BannerBackground.src})`,
+            backgroundPosition: 'center bottom',
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'contain',
+        }}>
+                <h2>Got a project in mind? Let's make something awesome <span className="span-pink">together</span>.</h2>
+                <Button arrow text="Hire Me" />
+            </div>
+        </div>
+    )
+}
