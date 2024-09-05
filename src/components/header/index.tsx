de9bd7b31898b41f40ac7a55c94eee92ecc3e371
João Lucas MAIN <joaolucasmmd@gmail.com>
2024-09-05 01:01:51 -0300

    feat: add header component

---
+++ src/components/header/index.tsx
@@ -0,0 +1,18 @@
+import styles from "./header.module.css"
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.menu}>
+        <nav className={styles.menuItens}>
+          <ul>
+            <li><a href="#About-Section">about</a></li>
+            <li><a href="#Services-Section">services</a></li>
+            <li><a href="#">works</a></li>
+            <li><a href="#">contact</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
+}
